31d7a68e7b89069be5e1093521c7c066d119e311
RoyFocker <liber.a.hernandez@gmail.com>
2020-09-10 15:01:32 -0500

    Fix release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Liber Adrián Hernández Abad',                   # Type in your name
   author_email = 'liber.a.hernandez@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/RoyFocker',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/RoyFocker/preBird/archive/v0.1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/RoyFocker/preBird/archive/v0.1.1.tar.gz',    # I explain this later on
   keywords = ['bird', 'birds', 'audio','preprocessing','processing'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
