8743e0b12198db22a01f0ede56a60617a90cd11f
RoyFocker <liber.a.hernandez@gmail.com>
2020-09-10 14:08:30 -0500

    First release

--- setup.py
+++ setup.py
@@ -8,17 +8,26 @@ setup(
   author = 'Liber Adrián Hernández Abad',                   # Type in your name
   author_email = 'liber.a.hernandez@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/RoyFocker',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/RoyFocker/preBird/archive/v0.1.tar.gz',    # I explain this later on
   keywords = ['bird', 'birds', 'audio','preprocessing','processing'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'numpy',
+          'matplotlib',
+          'scypi',
+          'sklearn',
+          'glob',
+          'pickle',
+          'statistics',
+          'skimage',
+          'playsound',
+          'copy',
+          'argparse'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: afl-3.0 License',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
