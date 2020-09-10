6f6719849ccf8e9adba8e1386571cc8b1c8ae019
RoyFocker <liber.a.hernandez@gmail.com>
2020-09-10 14:19:31 -0500

    Fix

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'prebird',         # How you named your package folder (MyLib)
   packages = ['prebird'],   # Chose the same as "name"
-  version = '0.1.2',      # Start with a small number and increase it with every change you make
+  version = '0.1',      # Start with a small number and increase it with every change you make
   license='afl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Intended to help preprocessing birds songs',   # Give a short description about your library
   author = 'Liber Adrián Hernández Abad',                   # Type in your name
   author_email = 'liber.a.hernandez@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/RoyFocker',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/RoyFocker/preBird/archive/v0.1.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/RoyFocker/preBird/archive/v0.1.0.tar.gz',    # I explain this later on
   keywords = ['bird', 'birds', 'audio','preprocessing','processing'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
