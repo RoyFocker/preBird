6b35d79bafb3e9f7ee5334ed770fca9b0062e26a
RoyFocker <liber.a.hernandez@gmail.com>
2020-09-10 15:00:23 -0500

    Fix release

---
+++ build/lib/prebird/__init__.py
@@ -0,0 +1,6 @@
+#Prebird
+#Coded by L. Adrián Hernández A.
+
+from prebird.scyspec import reader, sampler, get_spec, plotter,
+blackandwhite, adjustment, plotterbw, activity, intervals,
+index2samples, bird, outwavAves, outwavFondos, player, stats
--- prebird/__init__.py
+++ prebird/__init__.py
@@ -1,6 +1,4 @@
 #Prebird
 #Coded by L. Adrián Hernández A.
 
-from prebird.scyspec import reader, sampler, get_spec, plotter,
-blackandwhite, adjustment, plotterbw, activity, intervals,
-index2samples, bird, outwavAves, outwavFondos, player, stats
+from prebird.scyspec import test
